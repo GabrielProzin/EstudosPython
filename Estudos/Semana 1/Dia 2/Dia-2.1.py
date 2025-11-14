da11beef3431416b5cb1e2007586e969a415495c
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-11-14 07:51:27 -0300

    Respondendo dia 2 - básico

---
+++ Estudos/Semana 1/Dia 2/Dia-2.1.py
@@ -0,0 +1,25 @@
+#📘 DIA 2 – Variáveis e Tipos de Dados (int, float, str, bool)
+#2.1 — Básico:
+#Crie 4 variáveis representando informações suas:
+
+#nome
+#idade
+#altura
+
+#está_estudando_python (True ou False)
+#Depois exiba todas usando uma única linha de código.
+
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite a sua idade: "))
+altura = int(input("Digite a sua altura em centímetros: "))
+esta_estudando_python = True
+
+print(f"Eu me chamo {nome}, tenho {idade} anos e tenho {altura} centímetros de altura, e {esta_estudando_python}")
+
+
+#output:
+#PS C:\Users\Gabriel Mendes\Desktop\EstudosPython> & "C:/Users/Gabriel Mendes/AppData/Local/Programs/Python/Python313/python.exe" "c:/Users/Gabriel Mendes/Desktop/EstudosPython/Estudos/Semana 1/Dia1.py"
+#Digite o seu nome: Gabriel
+#Digite a sua idade: 21
+#Digite a sua altura em centímetros: 175
+#Eu me chamo Gabriel, tenho 21 anos e tenho 175 centímetros de altura, e True  
