2e27e1eff9557b229606ee9036081b2d62015fed
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-11-23 21:05:53 -0300

    adicionando o dia 12.3

--- Estudos/Semana 2/Dia 12/Dia-12.3.py
+++ Estudos/Semana 2/Dia 12/Dia-12.3.py
@@ -13,3 +13,31 @@ A senha deve obrigatoriamente conter:
 pelo menos 1 número
 pelo menos 1 símbolo
 '''
+
+import random
+import string
+
+maisculas = string.ascii_uppercase
+minuculas = string.ascii_lowercase
+numeros = string.digits
+simbolos = string.punctuation
+
+todos = maisculas + minuculas + numeros + simbolos
+senha = []
+
+senha.append(random.choice(numeros))
+senha.append(random.choice(simbolos))
+
+while len(senha) < 12:
+    novo = random.choice(todos)
+
+    if len(senha) >= 2 and senha[-1] == novo and senha[-2] == novo:
+        continue
+    else:
+        senha.append(novo)
+
+random.shuffle(senha)
+#senhaFinal = str(senha)
+senhaFinal = "".join(senha)
+
+print(senhaFinal)
