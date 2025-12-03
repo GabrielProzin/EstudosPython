65839f2d99b4ec03617ae8abc6ed30a2a0bb36b5
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-12-02 23:37:16 -0300

    adicionando o dia 18.3

--- Estudos/Semana 3/Dia 18/Dia-18.3.py
+++ Estudos/Semana 3/Dia 18/Dia-18.3.py
@@ -25,3 +25,22 @@ Requisitos:
 Usar funções de NumPy para tudo (evitar loops).
 Mostrar os resultados bem formatados.
 '''
+
+import numpy as np
+
+temperaturas = np.random.normal(loc=25, scale=3, size=1000)
+
+temp_media = np.mean(temperaturas)
+media_desvio = np.std(temperaturas)
+temp_min = np.min(temperaturas)
+temp_max = np.max(temperaturas)
+qntd_acima_30 = np.sum(temperaturas > 30)
+qntd_abaixo_20 = np.sum(temperaturas < 20)
+
+print(temp_media)
+print(media_desvio)
+print(temp_min)
+print(temp_max)
+print(qntd_acima_30)
+print(qntd_abaixo_20)
+
