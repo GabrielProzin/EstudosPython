919930df0be79f9cea7405eb4e9a17b8d8078fdc
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-12-04 23:24:27 -0300

    Adicionando o dia 20.2 e os arquivos gerados à partir dele

--- Estudos/Semana 3/Dia 20/Dia-20.2.py
+++ Estudos/Semana 3/Dia 20/Dia-20.2.py
@@ -15,3 +15,35 @@ Calcular a média e criar coluna media.
 Salvar apenas nome e media em um novo CSV medias.csv.
 Gerar um arquivo de texto aprovados.txt só com nomes dos alunos que tiveram média ≥ 7.
 '''
+
+import pandas as pd
+
+medias = {}
+
+with open("notas_alunos.csv", "r", encoding="utf-8") as file:
+    dados = pd.read_csv(file)
+
+    dados["media"] = ((dados["nota1"] + dados["nota2"] + dados["nota3"]) / 3).round(1)
+    media = dados[["nome", "media"]]
+
+print(media)
+
+media.to_csv("medias.csv", index=False)
+
+aprovados = []
+
+with open("medias.csv", "r") as file:
+    medias = pd.read_csv(file)
+
+    for indice, linha in medias.iterrows():
+        nome = linha["nome"]
+        media = linha["media"]
+        if media >= 7:
+            aprovados.append(nome)
+        with open("aprovados.txt", "w") as file:
+            for aluno in aprovados:
+                file.write(aluno + "\n")
+
+print(aprovados)
+
+
