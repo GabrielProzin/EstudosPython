91a206f81d4cc398ace0fdba110edadc0a45c48b
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-12-05 22:57:10 -0300

    Adicionando o dia 20.3

--- Estudos/Semana 3/Dia 20/Dia-20.3.py
+++ Estudos/Semana 3/Dia 20/Dia-20.3.py
@@ -22,3 +22,29 @@ Salvar:
 resumo em resumo_vendas.txt;
 gráfico em arquivo de imagem grafico_vendas.png.
 '''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+vendas = pd.read_csv("vendas_completo.csv", sep=",", encoding="utf-8")
+
+vendas["data"] = pd.to_datetime(vendas["data"])
+
+#print(vendas["data"].dtype)
+
+vendas["mes"] = vendas["data"].dt.month
+vendas["total"] = vendas["quantidade"] * vendas["preco_unitario"]
+vendas["total_vendas"] = vendas.groupby("mes")["total"].sum()
+
+#print(vendas["total_vendas"].max())
+
+categoria_mais_vendida = vendas.groupby("categoria")["quantidade"].sum()
+
+#print(categoria_mais_vendida.idxmax())
+
+plt.plot(categoria_mais_vendida)
+plt.xlabel("Categorias")
+plt.ylabel("Vendas")
+#plt.plot()
+plt.title("Total de vendas por categoria")
+plt.show()
