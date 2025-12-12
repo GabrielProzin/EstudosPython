e2eead5142b21afce3aa553139502855c31fac2a
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-12-11 22:12:29 -0300

    Corrigindo o dia 22.1

--- Estudos/Semana 4/Dia 22/Dia-22.1.py
+++ Estudos/Semana 4/Dia 22/Dia-22.1.py
@@ -23,11 +23,9 @@ Mostre esse nome de arquivo na tela (não precisa criar o arquivo ainda).
 from datetime import datetime
 
 data_agora = datetime.now()
-
 print(data_agora)
 
 data_formatada = data_agora.strftime("%Y-%m-%d")
-
 print(data_formatada)
 
 nome_arquivo = f"backup-{data_formatada}.txt"
