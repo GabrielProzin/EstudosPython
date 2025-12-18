4bf1db15342412d4ef5352ac6b75a854e2128f6a
gabrielprozin <gabrielalexandrino2004@gmail.com>
2025-12-17 23:11:11 -0300

    Adicionando o dia 25.2

--- Estudos/Semana 4/Dia 25/Dia-25.2.py
+++ Estudos/Semana 4/Dia 25/Dia-25.2.py
@@ -8,24 +8,54 @@ import requests
 Conceitos importantes:
 
 juntar lógica + API
-
 separar código em funções (obter_cotacao(moeda), converter(moeda, valor))
-
-Enunciado:
 
 Monte um menu:
 
 1 - Converter USD → BRL
-
 2 - Converter EUR → BRL
-
 0 - Sair
 
 Ao escolher 1 ou 2:
 
 busque a cotação na API
-
 peça o valor
-
 exiba o resultado formatado com 2 casas decimais.
 '''
+#https://open.er-api.com/v6/latest/
+
+import requests
+
+def obter_cotacao(moeda):
+    url = f"https://open.er-api.com/v6/latest/{moeda}"
+    response = requests.get(url)
+    dados = response.json()
+    return dados["rates"]["BRL"]
+    
+def converter(cotacao, valor):
+    return valor * cotacao
+
+while True:
+    print("Escolha alguma das seguintes opções do menu: \n")
+    print("1 - Converter USD → BRL")
+    print("2 - Converter EUR → BRL")
+    print("0 - Sair")
+    opcao = int(input(": "))
+
+    if opcao == 1:
+        moeda = "USD"
+        valor = float(input("Digite quantos dolares deseja converter: "))
+        cotacao = obter_cotacao(moeda)
+        conversao = converter(valor, cotacao)
+        print(f"{valor} dolares convertido são R${conversao:.2f} reais")
+
+    if opcao == 2:
+        moeda = "EUR"
+        valor = float(input("Digite quantos euros deseja converter: "))
+        cotacao = obter_cotacao(moeda)
+        conversao = converter(valor, cotacao)
+        print(f"{valor} euros convertido são R${conversao:.2f} reais")
+    
+    if opcao == 0:
+        print("Saindo...")
+        break
